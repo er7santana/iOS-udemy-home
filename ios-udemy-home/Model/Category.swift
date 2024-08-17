5da343ef7dc6e1e4d08e4e0572dd95b2053809da
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-17 10:36:50 -0300

    lesson 18 - add categories View

--- ios-udemy-home/Model/Category.swift
+++ ios-udemy-home/Model/Category.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+enum Category: String, CaseIterable {
+    
+    case development
+    case business
+    case officeProductivity
+    case healthAndFitness
+    case teachingAndAcademics
+    case financeAndAccounting
+    case itAndSoftware
+    case personalDevelopment
+    case marketing
+    case protographyAndVideo
+    case design
+    case lifestyle
+    case music
+    
+}
--- ios-udemy-home/SwiftUIView/CategoriesView.swift
+++ ios-udemy-home/SwiftUIView/CategoriesView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct CategoriesView: View {
+    
+    let titles: [String]
+    
+    var midPoint: Int {
+        return Int(titles.count / 2)
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyVStack(alignment: .leading, spacing: 8) {
+                HStack {
+                    ForEach(titles[..<midPoint], id: \.self) { title in
+                        CategoryButton(title: title)
+                    }
+                }
+                HStack {
+                    ForEach(titles[midPoint...], id: \.self) { title in
+                        CategoryButton(title: title)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CategoryButton: View {
+    
+    let title: String
+    var onTap: (() -> Void)?
+    
+    var body: some View {
+        Button {
+            onTap?()
+        } label: {
+            Text(title)
+                .padding(12)
+                .font(.system(size: 10, weight: .semibold))
+                .foregroundColor(.black)
+                .background(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(.black, lineWidth: 1.0))
+        }
     }
 }
 
 #Preview {
-    CategoriesView()
+    CategoriesView(titles: Category.allCases.map { $0.rawValue.camelCaseToEnglish.useShortAndFormat })
 }
