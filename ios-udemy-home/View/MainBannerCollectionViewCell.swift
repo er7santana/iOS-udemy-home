abcbeed7401444d2e1a810dfa3d3d164e69b9cde
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-17 07:10:49 -0300

    lesson 7: adding mainBannerViewControllerCell

--- ios-udemy-home/View/MainBannerCollectionViewCell.swift
+++ ios-udemy-home/View/MainBannerCollectionViewCell.swift
@@ -5,4 +5,22 @@
 //  Created by Eliezer Rodrigo Beltramin de Sant Ana on 17/08/24.
 //
 
-import Foundation
+import SwiftUI
+
+final class MainBannerCollectionViewCell: UICollectionViewCell {
+    
+    private var hostingController: UIHostingController<MainBannerView>!
+    
+    func configure(imageLink: String, title: String, caption: String) {
+        guard hostingController == nil else { return }
+        let mainBannerView = MainBannerView(imageLink: imageLink, title: title, caption: caption)
+        hostingController = UIHostingController(rootView: mainBannerView)
+        guard let hostingController else { return }
+        addSubview(hostingController.view)
+        hostingController.view.clipsToBounds = true
+        hostingController.view.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+}
